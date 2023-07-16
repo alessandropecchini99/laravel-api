feabb380e04541bfa22f351d0e56bd2b19f83016
alessandropecchini99 <pecchini99@gmail.com>
2023-07-17 01:10:09 +0200

    add api

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::with('type', 'technologies')->paginate(5);
+        $posts = Post::with('type', 'technologies')->paginate(4);
 
         return response()->json($posts);
     }
